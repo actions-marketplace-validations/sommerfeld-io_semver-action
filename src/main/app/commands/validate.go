f96a17817499a18dba484a698a4de727f5e16cf5
sebastian <sebastian@sommerfeld.io>
2023-07-21 12:41:27 +0000

    show error messages when flag is present

--- src/main/app/commands/validate.go
+++ src/main/app/commands/validate.go
@@ -9,11 +9,12 @@ import (
 
 // NewCmdConfig initializes the `tron config` command.
 func NewCmdValidate() *cobra.Command {
+	const SHOW_ERRORS_FLAG = "show-errors"
 
 	cmd := &cobra.Command{
 		Use:   "validate",
 		Short: "Validate given version string",
-		Long:  "Validate given version string against semantic versioning rules",
+		Long:  "Validate given version string against semantic versioning rules - true = version is valid, false = version does not comply with rules for sommerfeld-io projects",
 		Args:  cobra.ExactArgs(1),
 
 		Run: func(cmd *cobra.Command, args []string) {
@@ -21,12 +22,14 @@ func NewCmdValidate() *cobra.Command {
 
 			isValid, err := services.IsValid(version)
 
-			if err != nil { // && --show-errors
+			if err != nil && cmd.Flags().Lookup(SHOW_ERRORS_FLAG).Changed {
 				fmt.Println(err)
 			}
 			fmt.Println(isValid)
 		},
 	}
+
+	cmd.Flags().Bool(SHOW_ERRORS_FLAG, false, "print error messages in case a validation fails")
 
 	return cmd
 }

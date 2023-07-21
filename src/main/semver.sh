089fbbcb44315c1c01d2c07c3c3adfe4bcf3d7bb
sebastian <sebastian@sommerfeld.io>
2023-07-21 08:22:50 +0000

    updated header docs

--- src/main/semver.sh
+++ src/main/semver.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # @file semver.sh
-# @brief Build, test and run the semver app locally.
+# @brief Build, test and run the semver app and Docker image locally.
 #
-# @description Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-# eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-# eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-# sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-# elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-# diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-# gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+# @description This script is designed to build and test the Semver Docker image using Docker Compose. It
+# performs a series of essential steps, including initializing Go modules, running Go tests, building the
+# binary from the Go app, and creating the Docker image with the binary. Additionally, the script executes
+# tests within several containers to ensure the functionality of the Docker image.
 #
 # === Script Arguments
 #
@@ -20,6 +17,12 @@
 # ```
 # ./semver.sh --help
 # ```
+#
+# == Prerequisites
+#
+# The only prerequisite is a working a Docker installation. Docker compose is shipped alongside Docker. It
+# has been developd and tested with Docker version 24.0.2 on top of Ubuntu 22.10. With Docker installed,
+# users can execute the script to build and test the Semver Docker image without any additional dependencies.
 
 set -o errexit
 set -o pipefail
